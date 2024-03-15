68ece4252d859fe9a69a8d72f1e51d8bdfb19391
Andreas Fuchs <asf@boinkor.net>
2024-03-15 09:11:49 -0400

    Format nix code with alejandra

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.generate-go-sri.flakeModules.default
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, pkgs, lib, ... }: {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
         go-sri-hashes.default = {};
 
         packages = {
@@ -13,7 +19,7 @@
           hoopsnake = pkgs.buildGo122Module rec {
             pname = "hoopsnake";
             version = "0.0.0";
-            vendorHash = (builtins.readFile ./default.sri);
+            vendorHash = builtins.readFile ./default.sri;
             subPackages = ["cmd/hoopsnake"];
             src = lib.sourceFilesBySuffices (lib.sources.cleanSource ./.) [".go" ".mod" ".sum"];
             meta.mainProgram = "hoopsnake";
@@ -27,5 +33,5 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
     generate-go-sri.url = "github:antifuchs/generate-go-sri";
- };
+  };
 }
