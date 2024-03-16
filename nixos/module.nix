9f509bcaae5283d2e849c132868e1451c43e3c2d
Andreas Fuchs <asf@boinkor.net>
2024-03-16 10:12:59 -0400

    Test progress: alice comes up, is reachable!

--- nixos/module.nix
+++ nixos/module.nix
@@ -8,12 +8,21 @@
   ...
 }: {
   options = with lib; {
-    boot.initrd.hoopsnake = let
-      flakePkgs = withSystem ({config, ...}: config.packages);
+    boot.initrd.network.hoopsnake = let
+      hoopsnake = withSystem pkgs.stdenv.targetPlatform.system ({config, ...}: config.packages.default);
     in {
       enable = mkEnableOption "Enable the hoopsnake SSH server";
-      package = mkPackageOption flakePkgs "hoopsnake" {
-        default = ["inputs.hoopsnake.packages.default"];
+      package = mkOption {
+        description = "The hoopsnake package";
+        type = types.package;
+        default = hoopsnake;
+        defaultText = lib.literalExpression "inputs.hoopsnake.packages.''${system}.default";
+      };
+
+      includeSSLBundle = mkOption {
+        description = "Whether to include the SSL certificate bundle data in the initrd.";
+        default = true;
+        type = types.bool;
       };
 
       ssh = {
@@ -45,6 +54,8 @@
           description = "Environment file setting TS_AUTHKEY, TS_API_KEY or TS_API_CLIENT_ID & TS_API_CLIENT_SECRET. These are secrets, so shouldn't live in the nix store.";
           type = types.path;
         };
+        tsnetVerbose = mkEnableOption "verbose logging from the tsnet package";
+
         cleanup = {
           deleteExisting = mkOption {
             description = "Whether to delete existing nodes with the configured hoopsnake SSH server's name";
@@ -63,25 +74,35 @@
   };
 
   config = let
-    cfg = config.boot.initrd.hoopsnake;
+    cfg = config.boot.initrd.network.hoopsnake;
   in
     lib.mkIf cfg.enable {
       boot.initrd.network.postCommands = ''
+        echo "Starting hoopsnake..."
         . /etc/hoopsnake/tailscale/environment
         export TS_AUTHKEY TS_API_KEY TS_API_CLIENT_ID TS_API_CLIENT_SECRET TS_BASE_URL
 
         ${lib.getExe cfg.package} -name ${lib.escapeShellArg cfg.tailscale.name} \
-           -tags=${lib.escapeShellArg (lib.concatStringsSep cfg.tailscale.tags ",")} \
+           -tsnetVerbose=${lib.boolToString cfg.tailscale.tsnetVerbose} \
+           -tags=${lib.escapeShellArg (lib.concatStringsSep "," cfg.tailscale.tags)} \
            -deleteExisting=${lib.boolToString cfg.tailscale.cleanup.deleteExisting} \
            -maxNodeAge=${lib.escapeShellArg cfg.tailscale.cleanup.maxNodeAge} \
            -authorizedKeys=/etc/hoopsnake/ssh/authorized_keys \
            -hostKey=/etc/hoopsnake/ssh/host_key \
            ${lib.escapeShellArg cfg.ssh.shell} &
       '';
-      boot.initrd.secrets = {
-        "/etc/hoopsnake/ssh/host_key" = cfg.ssh.privateHostKey;
-        "/etc/hoopsnake/ssh/authorized_keys" = cfg.ssh.authorizedKeysFile;
-        "/etc/hoopsnake/tailscale/environment" = cfg.tailscale.environmentFile;
-      };
+      boot.initrd.secrets = lib.mkMerge [
+        {
+          "/etc/hoopsnake/ssh/host_key" = cfg.ssh.privateHostKey;
+          "/etc/hoopsnake/ssh/authorized_keys" = cfg.ssh.authorizedKeysFile;
+          "/etc/hoopsnake/tailscale/environment" = cfg.tailscale.environmentFile;
+        }
+        (lib.mkIf cfg.includeSSLBundle {
+          "/etc/ssl/ca-bundle.crt" = config.environment.etc."ssl/certs/ca-bundle.crt".source;
+          "/etc/ssl/ca-certificates.crt" = config.environment.etc."ssl/certs/ca-certificates.crt".source;
+          "/etc/pki/tls/certs/ca-bundle.crt" = config.environment.etc."pki/tls/certs/ca-bundle.crt".source;
+          "/etc/ssl/trust-source" = config.environment.etc."ssl/trust-source".source;
+        })
+      ];
     };
 }
--- nixos/tests/flake-part.nix
+++ nixos/tests/flake-part.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  inputs,
-  ...
-}: {
+{self, ...}: {
   perSystem = {
     config,
     pkgs,
@@ -30,15 +26,17 @@
     envFiles = {
       apiKey = pkgs.writeText "apikey-envfile" ''
         TS_API_KEY=OVehtREWXA._bTQCNek9SsZk5Jy9aIUpGWNWXo61rTh0QTg-_MUCv4
+        TS_BASE_URL=https://headscale
       '';
       authKey = pkgs.writeText "apikey-envfile" ''
         TS_AUTHKEY=e01aa49edded75748e17903330e3f18c25496b47360ffdec
+        TS_BASE_URL=https://headscale
       '';
       # Unfortunately, headscale doesn't support oauth client id/secret yet.
     };
     dbDump = pkgs.writeText "generated_keys.sql" ''
-      INSERT INTO pre_auth_keys VALUES(1,'e01aa49edded75748e17903330e3f18c25496b47360ffdec',1,0,0,0,datetime(),datetime('now','+1 hour'));
-        INSERT INTO api_keys VALUES(1,'OVehtREWXA',X'24326124313024436f72666574456a30774973344c3745616e697234756e2e536c6746654e71527030694448596153594968355a65374f6742513061',datetime(),datetime('now','+1 hour'),NULL);
+      INSERT INTO pre_auth_keys VALUES(90,'e01aa49edded75748e17903330e3f18c25496b47360ffdec',1,0,0,0,datetime(),datetime('now','+1 hour'));
+      INSERT INTO api_keys VALUES(90,'OVehtREWXA',X'24326124313024436f72666574456a30774973344c3745616e697234756e2e536c6746654e71527030694448596153594968355a65374f6742513061',datetime(),datetime('now','+1 hour'),NULL);
     '';
     hostkey = pkgs.runCommand "hostkey" {buildInputs = [pkgs.openssh];} ''
       mkdir $out
@@ -48,16 +46,70 @@
       mkdir $out
       ssh-keygen -N "" -t ed25519 -f $out/client
     '';
-    bootloader = {
+    bootloader = {config, ...}: {
       virtualisation.useBootLoader = true;
       virtualisation.useEFIBoot = true;
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
       environment.systemPackages = [pkgs.efibootmgr];
+      boot.kernelParams = [
+        "ip=${config.networking.primaryIPAddress}:::255.255.255.0::eth1:off"
+      ];
+      boot.initrd.preLVMCommands = ''
+        while true; do
+            if [ -f fnord ]; then
+              poweroff
+            fi
+            echo "Waiting to be told to shutdown"
+            sleep 1
+          done
+      '';
+      boot.initrd.network = {
+        enable = true;
+        udhcpc.enable = false;
+      };
+      boot.initrd.secrets = {
+        "/etc/hosts" = "${config.environment.etc.hosts.source}";
+      };
+      security.pki.certificateFiles = ["${tls-cert}/cert.pem"];
     };
+    headscalePort = 8080;
     stunPort = 3478;
+
     headscale = {config, ...}: {
+      services = {
+        headscale = {
+          enable = true;
+          port = headscalePort;
+          settings = {
+            server_url = "https://headscale";
+            ip_prefixes = ["100.64.0.0/10"];
+            derp.server = {
+              enabled = true;
+              region_id = 999;
+              stun_listen_addr = "0.0.0.0:${toString stunPort}";
+            };
+          };
+        };
+        nginx = {
+          enable = true;
+          virtualHosts.headscale = {
+            addSSL = true;
+            sslCertificate = "${tls-cert}/cert.pem";
+            sslCertificateKey = "${tls-cert}/key.pem";
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:${toString headscalePort}";
+              proxyWebsockets = true;
+            };
+          };
+        };
+      };
+      networking.firewall = {
+        allowedTCPPorts = [80 443];
+        allowedUDPPorts = [stunPort];
+      };
       environment.systemPackages = [
+        pkgs.headscale
         (pkgs.writeShellApplication {
           name = "import-pregenerated-keys";
           runtimeInputs = [pkgs.sqlite];
@@ -65,35 +117,7 @@
             sqlite3 /var/lib/headscale/db.sqlite <${dbDump}
           '';
         })
-        pkgs.headscale
       ];
-      services.headscale = {
-        enable = true;
-        settings = {
-          ip_prefixes = ["100.64.0.0/10"];
-          derp.server = {
-            enabled = true;
-            region_id = 999;
-            stun_listen_addr = "0.0.0.0:${toString stunPort}";
-          };
-        };
-      };
-      networking.firewall = {
-        allowedTCPPorts = [443 80];
-        allowedUDPPorts = [stunPort];
-      };
-      services.nginx = {
-        enable = true;
-        virtualHosts.headscale = {
-          addSSL = true;
-          sslCertificate = "${tls-cert}/cert.pem";
-          sslCertificateKey = "${tls-cert}/key.pem";
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
-            proxyWebsockets = true;
-          };
-        };
-      };
     };
   in {
     checks =
@@ -104,48 +128,47 @@
           name = "hoopsnake-starts";
           nodes = {
             inherit headscale;
+            peer = {
+              services.tailscale.enable = true;
+              security.pki.certificateFiles = ["${tls-cert}/cert.pem"];
+            };
             alice = {...}: {
               imports = [bootloader self.nixosModules.default];
-              boot.initrd.hoopsnake = {
-                enable = true;
-                ssh = {
-                  authorizedKeysFile = "${clientKey}/client.pub";
-                  privateHostKey = "${hostkey}/hostkey";
-                };
-                tailscale = {
-                  name = "alice-boot";
-                  tags = ["tag:hoopsnake"];
-                  environmentFile = envFiles.authKey;
+              boot.initrd.network = {
+                hoopsnake = {
+                  enable = true;
+                  ssh = {
+                    authorizedKeysFile = "${clientKey}/client.pub";
+                    privateHostKey = "${hostkey}/hostkey";
+                  };
+                  tailscale = {
+                    name = "alice-boot";
+                    tags = ["tag:hoopsnake"];
+                    environmentFile = envFiles.authKey;
+                    tsnetVerbose = true;
+                  };
                 };
               };
-            };
-            bob = {
-              services.tailscale.enable = true;
-              systemd.services.tailscaled.serviceConfig.Environment = ["TS_NO_LOGS_NO_SUPPORT=true"];
-              security.pki.certificateFiles = ["${tls-cert}/cert.pem"];
             };
           };
 
           testScript = ''
-            headscale.start()
-            bob.start()
+            for node in [headscale, peer]:
+                node.start()
             headscale.wait_for_unit("headscale")
-            headscale.succeed("headscale users create alice")
+            headscale.wait_for_open_port(443)
+
+            # Create headscale user and preauth-key
+            headscale.succeed("headscale users create peer")
+            authkey = headscale.succeed("headscale preauthkeys -u peer create --reusable")
+
+            # Connect peers
+            up_cmd = f"tailscale up --login-server 'https://headscale' --auth-key {authkey}"
+            peer.execute(up_cmd)
             headscale.succeed("import-pregenerated-keys")
 
-            headscale.succeed("headscale users create bob")
-            authkey = headscale.succeed("headscale preauthkeys -u bob create --reusable")
-            headscale.wait_for_open_port(8080)
-            # alice.start()
-
-            keys = headscale.succeed("headscale preauthkeys list -u bob")
-            print(keys)
-            print(authkey)
-
-            bob.wait_for_unit("tailscaled")
-            bob.succeed("ping -c 1 headscale")
-            bob.succeed("tailscale up --login-server 'https://headscale' --auth-key {authkey}")
-            bob.wait_until_succeeds("tailscale ping alice-boot", timeout=30)
+            alice.start()
+            peer.wait_until_succeeds("tailscale ping alice-boot", timeout=30)
           '';
         };
       };
