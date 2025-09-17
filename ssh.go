3b0705be015cab57ab940b1469b6bf177fb4f43f
Andreas Fuchs <asf@boinkor.net>
2025-09-17 08:44:35 -0400

    Fix lints(staticcheck): remove unnecessary Server field reference

--- ssh.go
+++ ssh.go
@@ -43,7 +43,7 @@ func (s *TailnetSSH) setupAuthorizedKeys() error {
 		}
 	}
 	if len(s.authorizedPubKeys) > 0 {
-		s.Server.PublicKeyHandler = s.validatePubkey
+		s.PublicKeyHandler = s.validatePubkey
 	}
 	return nil
 }
@@ -78,7 +78,7 @@ func (s *TailnetSSH) setupHostKey() error {
 // errors are fatal.
 func (s *TailnetSSH) Run(ctx context.Context) error {
 	var err error
-	s.Server.Handler = s.handle
+	s.Handler = s.handle
 
 	srv, err := s.tsnetServer(ctx)
 	if err != nil {
@@ -113,9 +113,9 @@ func (s *TailnetSSH) Run(ctx context.Context) error {
 	log.Printf("starting ssh server on port :22...")
 	go func() {
 		<-ctx.Done()
-		_ = s.Server.Close()
+		_ = s.Close()
 	}()
-	err = s.Server.Serve(listener)
+	err = s.Serve(listener)
 	if err != nil && ctx.Err() == nil {
 		return fmt.Errorf("ssh server failed serving: %w", err)
 	}
